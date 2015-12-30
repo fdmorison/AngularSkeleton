ed10f47fbdc3be0c6aee8b82af3ddce36c20d05f
fdmorison <fabricio.dmorison@gmail.com>
2015-12-30 16:53:59 -0400

    Added package.json and usemin plugin to build

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,61 +1,66 @@
 module.exports = function(grunt) {
 
+    require('load-grunt-tasks')(grunt);
+
     grunt.initConfig({
 
-        srcDir : "src/webapp",
+        pkg      : grunt.file.readJSON('package.json'),
+        srcDir   : "src/webapp",
         buildDir : "target",
+        stageDir : "<%=buildDir%>/.tmp",
 
         clean : {
             build : [ "<%=buildDir%>/" ],
+            stage : [ "<%=stageDir%>/" ]
+        },
+
+        copy : {
+            dist: {
+                files:[
+                    {
+                        src : '<%=srcDir%>/index.html',
+                        dest: '<%=buildDir%>/index.html'
+                    },
+                    {
+                        expand: true,
+                        cwd: '<%=srcDir%>/img',
+                        src: ['**'], 
+                        dest: '<%=buildDir%>/img'
+                    },
+                    {
+                        expand: true,
+                        cwd: '<%=srcDir%>/lib/bootstrap-3.3.6/fonts',
+                        src: '**',
+                        dest: '<%=buildDir%>/fonts'
+                    }
+                ]
+            }
+        },
+
+        useminPrepare: {
+            html: '<%=buildDir%>/index.html',
+            options: {
+                root: '<%=srcDir%>',
+                dest: '<%=buildDir%>',
+                staging: '<%=stageDir%>'
+            }
+        },
+
+        usemin: {
+            html: '<%=buildDir%>/index.html'
         },
 
         cssmin : {
             options : {
-                shorthandCompacting : false,
-                roundingPrecision : -1
-            },
-            target : {
-                files : {
-                    'target/css/style.css' : [ 
-                        '<%=srcDir%>/lib/bootstrap*/css/bootstrap.min.css',
-                        '<%=srcDir%>/css/base.css', 
-                        '<%=srcDir%>/css/components.css',
-                        '<%=srcDir%>/css/images.css', 
-                        '<%=srcDir%>/css/fonts.css',
-                        '<%=srcDir%>/css/layout.css', 
-                    ]
-                }
+                shorthandCompacting: false,
+                roundingPrecision: -1,
+                keepSpecialComments: 0
             }
         },
 
-        uglify : {
-            src : {
-                options : {
-                    mangle : false
-                },
-                files : {
-                    'target/js/app.min.js' : [ 
-                        '<%=srcDir%>/modules/utils.js',
-                        '<%=srcDir%>/modules/components/**/*.js',
-                        '<%=srcDir%>/modules/services/**/*.js',
-                        '<%=srcDir%>/modules/pages/**/*.js' 
-                    ]
-                }
-            },
-            lib : {
-                options : {
-                    mangle : false
-                },
-                files : {
-                    'target/js/lib.min.js' : [ 
-                        '<%=srcDir%>/lib/jquery-2.1.4.min.js',
-                        '<%=srcDir%>/lib/bootstrap-3.3.6/js/bootstrap.min.js',
-                        '<%=srcDir%>/lib/angular-1.4.8/angular.min.js',
-                        // '<%=srcDir%>/lib/lib/angular-1.4.8/angular-mocks.js',
-                        '<%=srcDir%>/lib/lib/angular-1.4.8/angular-resource.min.js',
-                        '<%=srcDir%>/lib/angular-ui-0.2.15/angular-ui-router.js'
-                     ]
-                }
+        uglify: {
+            options: {
+                mangle: false
             }
         },
 
@@ -63,56 +68,37 @@ module.exports = function(grunt) {
             index_html : {
                 options : {
                     beautify : true,
-                    scripts : {
-                        'app' : 'target/js/app.min.js',
-                        'lib' : 'target/js/lib.min.js'
-                    },
-                    styles : {
-                        'app' : 'target/css/style.css'
-                    }
+                    allowUnknownTags: true
                 },
-                src : '<%=srcDir%>/index.html',
-                dest : 'target/',
+                src : '<%=buildDir%>/index.html',
+                dest: '<%=buildDir%>',
             }
         },
 
         replace : {
             index_html : {
-                src : [ 'target/index.html' ],
+                src : [ '<%=buildDir%>/index.html' ],
                 overwrite : true,
                 replacements : [ {
-                    from : 'data-ng-app="fakeBackend"',
-                    to : 'data-ng-app="main"',
+                    from : 'fakeBackend',
+                    to : 'main',
                 } ]
             }
-        },
-        
-        copy : {
-            img: {
-                expand: true, 
-                cwd: '<%=srcDir%>/img', 
-                src: ['**'], 
-                dest: 'target/img'
-            },
-            bootstrap_fonts: {
-                expand: true, 
-                cwd: '<%=srcDir%>/lib/bootstrap-3.3.6/fonts', 
-                src: '**', 
-                dest: 'target/fonts'
-            }
-        },
+        }
 
     });
 
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-html-build');
-    grunt.loadNpmTasks('grunt-text-replace');
+    grunt.registerTask('default', [
+        'clean:build',
+        'copy',
+        'useminPrepare',
+        'concat',
+        'cssmin',
+        'uglify',
+        'usemin',
+        'htmlbuild',
+        'replace',
+        'clean:stage',
+    ]);
 
-    grunt.registerTask('default', 
-            [ 'clean', 'copy', 'cssmin', 'uglify', 'htmlbuild', 'replace']
-    );
-  
 };
